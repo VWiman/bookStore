704925203dd7494d4ea3552fd088bf08f791af6b
why_code <119089018+VWiman@users.noreply.github.com>
2024-08-28 09:22:19 +0200

    Updated logo and split bookManager into a seperate component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import bookManager from "./bookManager";
 
 export default function App() {
 	let idCount = 1;
 	const [activeId, setActiveId] = useState(null);
 	const [dataList, setDatalist] = useState([]);
+	const [fetched, setFetched] = useState(false);
 
 	async function fetchBooks() {
 		const readOnly = process.env.READONLY;
@@ -15,6 +17,7 @@ export default function App() {
 				"X-Bin-Meta": "false",
 			},
 		};
+
 		console.log(settings);
 		const promise = await fetch(url, settings);
 		const response = await promise.json();
@@ -22,33 +25,11 @@ export default function App() {
 			console.log(book);
 			book.id = idCount++;
 		});
+		setFetched(true);
 		setDatalist((prevDataList) => [...prevDataList, ...response]);
 	}
 
-	function bookManager() {
-		const Create = (newBook) => {
-			newBook.id = idCount++;
-			setDatalist((prevDataList) => [...prevDataList, newBook]);
-		};
-
-		const Read = () => {
-			const item = dataList.find((book) => book.id === activeId);
-			return item;
-		};
-
-		const Update = (updatedBook) => {
-			setDatalist((prevDataList) =>
-				prevDataList.map((oldBook) => (oldBook.id === activeId ? { ...oldBook, ...updatedBook } : oldBook))
-			);
-		};
-
-		const Delete = () => {
-			setDatalist((prevDataList) => prevDataList.filter((oldBook) => oldBook.id !== activeId));
-		};
-		return { Create, Read, Update, Delete };
-	}
-
-	const { Create, Read, Update, Delete } = bookManager();
+	const { Create, Read, Update, Delete } = bookManager(activeId, dataList, idCount);
 
 	useEffect(() => {
 		fetchBooks();
@@ -57,7 +38,6 @@ export default function App() {
 	return (
 		<>
 			<header className="header-container">
-				<img src="/book.png" width={205} height={202} className="corner-img"></img>
 				<h1>The Book Corner</h1>
 			</header>
 			<main className="main-container">
@@ -73,26 +53,34 @@ export default function App() {
 							</tr>
 						</thead>
 						<tbody className="table-body">
-							{dataList.map((book) => (
-								<tr className="table-row" key={book.id} onClick={() => setActiveId(book.id)}>
-									<td className="cell">{book.title}</td>
-									<td className="cell">{book.author}</td>
-									<td className="cell">{book.year}</td>
-								</tr>
-							))}
+							{fetched
+								? dataList.map((book) => (
+										<tr className="table-row" key={book.id} onClick={() => setActiveId(book.id)}>
+											<td className="cell">{book.title}</td>
+											<td className="cell">{book.author}</td>
+											<td className="cell">{book.year}</td>
+										</tr>
+								  ))
+								: Array.from({ length: 20 }).map((_, index) => (
+										<tr className="table-row" key={`loading-${index}`}>
+											<td className="cell">Loading</td>
+											<td className="cell">---</td>
+											<td className="cell">---</td>
+										</tr>
+								  ))}
 						</tbody>
 					</table>
 				</div>
 				<div className="book-info-container">
 					<div className="image-stat-container">
 						<dl>
-							<img className="book-img" src={activeId ? Read().image + ".png" : ""}></img>
+							<img className="book-img" src={activeId ? "/images/" + Read().image + ".png" : "/images/select.png"} />
 						</dl>
 						<div className="stat-container">
 							<small className="book-info-header-sub">the facts</small>
 							<dl>
 								<dt>author</dt>
-								<dd>{activeId ? Read().author : "---"}</dd>
+								<dd>{activeId ? Read().author : ""}</dd>
 							</dl>
 							<dl>
 								<dt>year</dt>
@@ -114,7 +102,6 @@ export default function App() {
 					</div>
 					<div>
 						<h3 className="book-info-title">{activeId ? Read().title : ""}</h3>
-
 						<p className="book-info-description">{activeId ? Read().description : ""}</p>
 					</div>
 				</div>
